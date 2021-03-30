a365a7ad9c6764c2cbf125a16ada881264947c13
yleeeee <1020076136@qq.com>
2021-03-29 23:08:45 -0700

    update

---
+++ chapter17/p17_20.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+bool valid(const smatch &m) {
+	if (m[1].matched) {
+		return m[3].matched && (m[4].matched || m[4].str() == " ");
+	} else {
+		return !m[3].matched && m[4].str() == m[6].str();
+	}
+}
+
+int main()
+{
+	string phone = "(\\()?(\\d{3})([ _.])?(\\d{3})([ _.])?(\\d{4})";
+	regex re(phone, regex::icase);
+
+	string temp;
+	while (getline(cin, temp)) {
+		for (sregex_iterator it(temp.begin(), temp.end(), re), end_it; it != end_it; ++it) {
+			if (valid(*it)) {
+				cout << "valid:" << it->str() << endl;
+			} else {
+				cout << "valid:" << it->str() << endl;
+			}
+		}
+	}
+}
